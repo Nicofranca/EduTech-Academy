333bce3861e0e61a15668754606ba607684f879a
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-17 16:50:11 -0300

    feat: recolhe dado do cep

---
+++ script.js
@@ -0,0 +1,30 @@
+const formulario = document.getElementById('cep');
+const rua = document.getElementById('rua');
+const bairro = document.getElementById('bairro');
+const cidade = document.getElementById('cidade');
+const estado = document.getElementById('estado');
+
+formulario.addEventListener('input', (event) => {
+    event.preventDefault();
+
+    const cep = event.target.value;
+
+    console.log(cep);
+//viacep.com.br/ws/${cep}/json/
+    const urlViaCepApi = `https://viacep.com.br/ws/${cep}/json/`;
+
+    if(cep.length === 8){
+        fetch(urlViaCepApi)
+        .then((response) => response.json())
+        .then((data) => {
+
+            
+
+            console.log(data);
+
+        })
+    }
+
+    }
+)
+
